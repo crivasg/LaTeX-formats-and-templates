4a7348d588a4afb1eae7fc8f8f5f8450cd6158cb
Cesar Rivas <crivasg@gmail.com>
2012-10-06 21:59:58 -0500

    Added while loop to build.latex.figures.sh

--- scripts/build.latex.figures.sh
+++ scripts/build.latex.figures.sh
@@ -4,4 +4,10 @@ TMP_FILE='/tmp/latex_figures.tmp'
 
 find . \( -wholename "*figures/*.tex" -not -name *_* \) -type f > $TMP_FILE
 
-echo $TMP_FILE
+
+while read line
+	do
+		echo "Building " $line " ...";	
+	done < ${TMP_FILE}
+
+
