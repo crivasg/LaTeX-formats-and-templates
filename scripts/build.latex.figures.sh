fd279642f1c3a6218db56f60602fbdb95a6e09b3
Cesar Rivas <crivasg@gmail.com>
2012-10-06 22:08:47 -0500

    generete dvi name by changing the extension from .tex to .dvi

--- scripts/build.latex.figures.sh
+++ scripts/build.latex.figures.sh
@@ -1,13 +1,20 @@
 #!/bin/ksh
 
 TMP_FILE='/tmp/latex_figures.tmp'
+LATEX='latex -interaction=batchmode'
+DVIPDF='dvipdf'
 
 find . \( -wholename "*figures/*.tex" -not -name *_* \) -type f > $TMP_FILE
 
 
 while read line
 	do
-		echo "Building " $line " ...";	
+		DVI=`echo $line | sed 's/.tex/.dvi/'`
+		echo "Building " $line  $DVI " ...";
+	#	${LATEX} $line	
+	#	${LATEX} $line
+	#	${DVIPDF} $DVI
+		
 	done < ${TMP_FILE}
 
 
